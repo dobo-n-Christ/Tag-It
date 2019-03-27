ff858af7e30d602bb03f1b7dea13111a872bec8f
Sarah Folk <dobo_n_Christ@yahoo.com>
2019-03-27 19:38:36 +0100

    integrated ejs and wrote more tests

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 'use strict';
 
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
-app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('pages/dashboard');
+});
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('pages/signup');
+});
 
 if(require.main === module) {
     app.listen(process.env.PORT || 8080, function() {
--- test/test-server.js
+++ test/test-server.js
@@ -7,7 +7,7 @@ const app = require('../server');
 
 chai.use(chaiHttp);
 
-describe('URL test', function() {
+describe('index page', function() {
     // before(function() {
     //     return runServer;
     // });
@@ -17,6 +17,39 @@ describe('URL test', function() {
     it('should return a 200 status and HTML', function() {
         return chai.request(app)
         .get('/')
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+        });
+    });
+});
+
+describe('login page', function() {
+    it('should exist', function() {
+        return chai.request(app)
+        .get('/login')
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+        });
+    });
+});
+
+describe('signup page', function() {
+    it('should exist', function() {
+        return chai.request(app)
+        .get('/signup')
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+        });
+    });
+});
+
+describe('dashboard page', function() {
+    it('should exist', function() {
+        return chai.request(app)
+        .get('/dashboard')
         .then(function(res) {
             expect(res).to.have.status(200);
             expect(res).to.be.html;
